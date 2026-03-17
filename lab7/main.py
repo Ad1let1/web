cea1b786bc3b44b2d336b99ee25ecd754b6d8b01
Adilet Sergazyuly <a_sergazyuly@kbtu.kz>
2026-03-17 17:07:39 +0500

    lab7 task2

--- lab7/main.py
+++ lab7/main.py
@@ -1,9 +1,9 @@
 from models import Product, PhysicalProduct, DigitalProduct
 
 def main():
-    generic_item = Product("Basic Box", 5.0, "BOX-01")
+    generic_item = Product("Box", 5.0, "BOX-01")
     laptop = PhysicalProduct("Gaming Laptop", 1200.0, "LAP-99", 2.5)
-    software = DigitalProduct("Photo Editor Pro", 60.0, "SFT-12", True)
+    software = DigitalProduct("Photoshop Pro", 60.0, "SFT-12", True)
 
     catalog = [generic_item, laptop, software]
 
